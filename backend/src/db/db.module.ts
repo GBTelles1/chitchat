de16095a1d45216cf5f5995e560102fa0d6846d5
GBTelles1 <gbertelles@gmail.com>
2024-12-06 20:04:26 -0300

    feat: add new entities and modules to the backend configuration

--- backend/src/db/db.module.ts
+++ backend/src/db/db.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chat } from 'src/chats/entities/chat.entity';
+import { Message } from 'src/messages/entities/message.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
   imports: [
@@ -13,7 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         username: configService.get<string>('DATABASE_USER'),
         password: configService.get<string>('DATABASE_PASS'),
         database: configService.get<string>('DATABASE_NAME'),
-        entities: [],
+        entities: [User, Chat, Message],
         synchronize: true,
       }),
       inject: [ConfigService],
